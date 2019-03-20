fa3361279e489a08866bf713d0acfdc1add4b004
Nigel Bunner <nigelbunner@gmail.com>
2019-03-20 20:23:30 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,18 +4,18 @@ layout: null
 const staticCacheName = 'nigelbunner-cache-v3';
 
 const filesToCache = [
-    '/',
-    '/css/nb-styles.css',
-    '/img/nigel-walking--sm-mobile.jpg',
-    '/img/nigel-walking--lg-mobile.jpg',
-    '/img/nigel-walking--tablet.jpg',
-    '/img/nigel-walking--desktop.jpg',  
+    '/', 
     {% for post in site.posts limit:5 %}
       '{{ post.url }}',
     {% endfor %}
     {% for page in site.html_pages %}
       '{{ page.url }}',
     {% endfor %}
+        '/css/nb-styles.css',
+    '/img/nigel-walking--sm-mobile.jpg',
+    '/img/nigel-walking--lg-mobile.jpg',
+    '/img/nigel-walking--tablet.jpg',
+    '/img/nigel-walking--desktop.jpg'
 ];
 
 
@@ -35,7 +35,7 @@ self.addEventListener("activate", function(e){
     caches.keys().then(function(cacheNames){
       return Promise.all(
         cacheNames.filter(function(cacheName){
-          return cacheName.startsWith("gdad-s-river-static-")
+          return cacheName.startsWith("nigelbunner-cache-")
             && cacheName != staticCacheName;
         }).map(function(cacheName){
           return caches.delete(cacheName);
