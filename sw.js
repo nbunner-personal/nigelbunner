5ef294e4d6aaebc705f5abde8e030c516e93f8e5
Nigel Bunner <nigelbunner@gmail.com>
2020-05-10 22:24:55 +0100

    Fixes for lighthouse

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ---
 layout: null
 ---
-const staticCacheName = 'nigelbunner-cache-v5.40';
+const staticCacheName = 'nigelbunner-cache-v5.45';
 
 const filesToCache = [    
     {% for post in site.posts limit:5 %}
