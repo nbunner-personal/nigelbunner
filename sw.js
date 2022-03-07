62d521b8961ae9fc607992b90e68f6955bb94403
Nigel Bunner <nigelbunner@gmail.com>
2022-03-07 09:40:19 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ---
 layout: null
 ---
-const staticCacheName = 'nigelbunner-cache-v5.60';
+const staticCacheName = 'nigelbunner-cache-v5.61';
 
 const filesToCache = [    
     {% for post in site.posts limit:5 %}
